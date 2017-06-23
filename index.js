fcf1696fbf177d859a1c393023da198c461d8252
Iván Rodríguez Murillo <wantez@gmail.com>
2017-06-24 01:04:30 +0200

    WIP 4

--- index.js
+++ index.js
@@ -1,64 +1,25 @@
-process.title = 'weplay-discovery';
+process.title = 'weplay-discovery'
 
-const crypto = require('crypto');
-const uuid = require('node-uuid').v4();
-const logger = require('weplay-common').logger('weplay-discovery', uuid);
+const crypto = require('crypto')
+const uuid = require('node-uuid').v4()
+const logger = require('weplay-common').logger('weplay-discovery', uuid)
 
-const discoveryPort = process.env.DISCOVERY_PORT || 3010;
-const Discovery = require('weplay-common').Discovery;
+const discoveryPort = process.env.DISCOVERY_PORT || 3010
+const statusPort = process.env.STATUS_PORT || 8080
+const Discovery = require('weplay-common').Discovery
 
-const discovery = new Discovery().server({name: 'My Discovery', port: discoveryPort}, ()=> {
-    logger.info('My Discovery server listening', {
-        port: discoveryPort,
-        uuid: uuid
-    });
-});
+const discovery = new Discovery().server({name: 'My Discovery', port: discoveryPort, statusPort: statusPort}, () => {
+  logger.info('My Discovery server listening', {
+    port: discoveryPort,
+    uuid: uuid
+  })
+})
 
 
-var restify = require('restify');
-
-function lookup(req, res, next) {
-    res.send(discovery.lookup().map(service=> {
-        if (service.events) {
-            const events = service.events.map(e=> {
-                return (e.room) ? e.event + '#' + e.room : e.event;
-            });
-            const streams = service.events.filter(e=>e.room).map(e=> {
-                return e.room;
-            });
-            return {
-                name: service.name,
-                id: service.id,
-                version: service.version,
-                events: events,
-                streams: streams,
-                depends: service.depends
-            };
-        }
-        else {
-            return {name: service.name, id: service.id, version: service.version};
-        }
-    }).sort(function (a, b) {
-        return a.name > b.name;
-    }));
-    next();
+const discoveryUrl = process.env.DISCOVERY_URL
+if (discoveryUrl) {
+  var discoveryClient = new Discovery().client(options, _onConnect)
 }
 
-var server = restify.createServer({
-    formatters: {
-        'application/json': function (req, res, body, cb) {
-            return cb(null, JSON.stringify(body, null, '\t'));
-        }
-    }
-});
 
-server.pre(restify.pre.userAgentConnection());
-
-server.get('/lookup', lookup);
-server.head('/lookup', lookup);
-
-server.listen(8080, function () {
-    logger.info('Restify %s listening at %s', server.name, server.url);
-});
-
-require('weplay-common').cleanup(discovery.destroy.bind(discovery));
+require('weplay-common').cleanup(discovery.destroy.bind(discovery))
