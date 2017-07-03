ed8736dfd5d70d89a0b487b3d5a78ffd2ea28f95
Iván Rodríguez Murillo <wantez@gmail.com>
2017-07-03 19:12:59 +0200

    reconnect

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,7 @@ RUN npm install
 
 # Setup environment
 ENV NODE_ENV production
+ENV DISCOVERY_PORT 3010
 ENV WEPLAY_REDIS_URI "redis:6379"
 ENV WEPLAY_LOGSTASH_URI "logstash:5001"
 
--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 process.title = 'weplay-discovery'
 
-const crypto = require('crypto')
 const uuid = require('node-uuid').v4()
 const logger = require('weplay-common').logger('weplay-discovery', uuid)
 
 const discoveryPort = process.env.DISCOVERY_PORT || 3010
-const statusPort = process.env.STATUS_PORT || 8080
+const statusPort = process.env.STATUS_PORT || 8088
 const Discovery = require('weplay-common').Discovery
 
-const discovery = new Discovery().server({name: 'My Discovery', port: discoveryPort, statusPort: statusPort}, () => {
+const discovery = new Discovery().server({name: 'discovery', port: discoveryPort, statusPort: statusPort}, () => {
   logger.info('My Discovery server listening', {
     port: discoveryPort,
     uuid: uuid
   })
 })
 
-
-const discoveryUrl = process.env.DISCOVERY_URL
+const discoveryUrl = process.env.DISCOVERY_MASTER_URL
 if (discoveryUrl) {
-  var discoveryClient = new Discovery().client(options, _onConnect)
+  const EventBus = require('weplay-common').EventBus
+  var bus = new EventBus({
+    url: discoveryUrl,
+    port: discoveryPort,
+    statusPort: statusPort,
+    name: 'emu',
+    id: this.uuid,
+    serverListeners: {
+      'move': (socket, request) => {
+        // console.log('move Request', request)
+        if (this.emu) {
+          this.emu.move(request)
+        }
+      },
+      'streamJoinRequested': this.streamJoinRequested.bind(this),
+      'streamCreateRequested': this.streamCreateRequested.bind(this)
+    },
+    clientListeners: [
+      // {name: 'rom', event: 'connect', handler: this.onRomConnect.bind(this)},
+      // {name: 'rom', event: 'disconnect', handler: this.onRomDisconnect.bind(this)},
+      {name: 'gateway', event: 'move', handler: this.onMove.bind(this)},
+      {name: 'rom', event: 'data', handler: this.onRomData.bind(this)},
+      {name: 'rom', event: 'hash', handler: this.onRomHash.bind(this)},
+      {name: 'rom', event: 'state', handler: this.onRomState.bind(this)}]
+  }, () => {
+    logger.info('Discovery slave connected to discovery server', {
+      discoveryUrl: discoveryUrl,
+      uuid: this.uuid
+    })
+  })
 }
-
 
 require('weplay-common').cleanup(discovery.destroy.bind(discovery))
